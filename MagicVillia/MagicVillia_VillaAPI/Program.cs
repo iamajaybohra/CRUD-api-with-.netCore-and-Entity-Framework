603c4cae45709f38768dcdbf5a5f2dd551da7c25
Ajay Bohra <63140274+iamajaybohra@users.noreply.github.com>
2023-12-28 14:56:29 +0530

    Added Content negotiation

--- MagicVillia/MagicVillia_VillaAPI/Program.cs
+++ MagicVillia/MagicVillia_VillaAPI/Program.cs
@@ -1,8 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers().AddNewtonsoftJson();
+/* option.ReturnHttpNotAcceptable is used for content negotiation that means if the server accept on the xml and if the
+ * server does not satisfy this request, then HTTP 406 Not Acceptable status code.
+ * AddXmlDataContractSerializerFormatters method is used to configure the MVC (Model-View-Controller) framework to include 
+ * formatters for XML serialization using the DataContractSerializer.
+ * i.e, After adding this line, your API can handle requests and responses in both JSON and XML formats based on the client's 
+ * preferences specified in the "Accept" header of the HTTP request.
+ */
+builder.Services.AddControllers(option =>
+{
+    option.ReturnHttpNotAcceptable = true;
+}
+    ).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
